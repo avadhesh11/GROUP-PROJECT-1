a740d3878d5babae205a7edbddbb9e6563d02d41
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-06-01 14:57:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/GROUP-PROJECT-1/',
   plugins: [
     react(),
     tailwindcss()
