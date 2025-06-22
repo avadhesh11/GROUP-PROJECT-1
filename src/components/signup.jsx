95b2fd80c4fe0c2fd52abcd75702a5cd0a2af2d0
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-06-22 12:42:48 +0530

    otp aunthentication done

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 import img from '../assets/couple.png'
 import { Link ,useNavigate }from 'react-router-dom';
 function Sign(){
+
   const navigate=useNavigate();
    useEffect(() => {
     const token = Cookies.get("refreshToken");
@@ -12,6 +13,8 @@ function Sign(){
       navigate("/WeddingCategories");
     }
   }, []);
+  const [part,setpart]=useState(1);
+  const [otp,setotp]=useState("");
     const [data,setdata]=useState(
         {
             name:'',
@@ -27,7 +30,11 @@ function Sign(){
       [e.target.id]: e.target.value
     });
     };
-  const submit=async(e)=>{
+  const otpchange=(e)=>{
+  setotp(e.target.value);
+
+  }
+  const sendotp=async(e)=>{
     e.preventDefault();
     try{
           const response = await fetch('http://localhost:5000/auth/signup', {
@@ -42,9 +49,9 @@ function Sign(){
       const Data = await response.json();
 
       if (response.ok) {
-        alert('User registered successfully!');
-        navigate("/WeddingCategories");
-        console.log(Data);
+        alert('OTP sent succesfully');
+        setpart(2);
+       console.log(Data);
       } else {
         alert(Data.error);
         console.error(Data);
@@ -55,16 +62,46 @@ function Sign(){
     
     }
   };
+    const verifyOtp=async(e)=>{
+      e.preventDefault();
+ try{
+          const response = await fetch('http://localhost:5000/auth/verify-otp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+         body: JSON.stringify({ email: data.email, otp }),
+        credentials:'include'
+      });
+
+      const Data = await response.json();
+
+      if (response.ok) {
+        alert('user registered succesfully');
+         navigate("/WeddingCategories");
+       console.log(Data);
+      } else {
+        alert(Data.error);
+        console.error(Data);
+      }
+    } catch (err) {
+      console.error('Error:', err);
+      alert('Something went wrong.');
     
+    }
+    }
 return(
+
 <div className="sign">
    <div className="card1">
+
     <div className="left">
       
       <div className="text">
  <h1 >Your Perfect Wedding is waiting</h1>
  </div>
- <form id="signupForm" onSubmit={submit}>
+   {part===1 ? (
+ <form id="signupForm" onSubmit={sendotp} >
 
   <div className="input-group">
     <input type="text" id="name" placeholder="Full Name" required="" onChange={handlechange} />
@@ -74,15 +111,25 @@ return(
   </div>
   <div className="input-group">
        <input type="email" id="email" placeholder="Email" required=""  onChange={handlechange} />
+    
   </div>
+  
+  
   <div className="input-group">
     <input type="password" id="password" placeholder="Password" required=""  onChange={handlechange}/>
   </div>
-  <button type="submit" className="login-btn" id="login-btn">
-    Sign Up
-  </button>
+  <button type="submit" className="login-btn">Send OTP</button>
   
 </form>
+):(
+<form onSubmit={verifyOtp}>
+              <div className="input-group">
+                <input type="text" id="otp" placeholder="Enter OTP" required onChange={otpchange} />
+              </div>
+              <button type="submit" className="login-btn">Verify otp</button>
+            </form>
+          )
+}
 <div className="other">
 <Link  class="navi" to="/login" style={{color:'blue',textDecoration: 'underline', fontStyle: 'italic'}}>Already have an account?</Link>
 <p>or continue with-</p>
