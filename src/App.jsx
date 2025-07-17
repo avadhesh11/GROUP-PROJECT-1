d76ebd971ba583fdd73729d3e83556c00f7d3172
divyam <divyam.iiitv@gmail.com>
2025-07-17 17:54:13 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Sign from './components/signup.jsx';
-import Login from './components/login.jsx';
-import Cookies from 'js-cookie';
-import WeddingCategories from './components/WeddingCategories';
-import VenuePage from'./components/VenuePage.jsx';
-import ThemePage from'./components/ThemePage.jsx';
-import FoodPage from './components/FoodPage.jsx';
-import PhotographyPage from './components/PhotographyPage.jsx';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Sign from "./components/signup.jsx";
+import Login from "./components/login.jsx";
+import Cookies from "js-cookie";
+import WeddingCategories from "./components/WeddingCategories";
+import VenuePage from "./components/VenuePage.jsx";
+import ThemePage from "./components/ThemePage.jsx";
+import FoodPage from "./components/FoodPage.jsx";
+import PhotographyPage from "./components/PhotographyPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
-     <Routes>
-      <Route path="/" element={<Sign />} />
-      <Route path="/login" element={<Login />} />
-      <Route path= "/WeddingCategories" element={<WeddingCategories />}/>
-      <Route path= "/VenuePage" element={<VenuePage />}/>
-      <Route path= "/ThemePage" element={<ThemePage />}/>
-      <Route path= "/FoodPage" element={<FoodPage />}/>
-       <Route path= "/PhotographyPage" element={<PhotographyPage />}/>
-    </Routes>
-    
+    <>
+      <Routes>
+        <Route path="/" element={<Sign />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/WeddingCategories" element={<WeddingCategories />} />
+        <Route path="/VenuePage" element={<VenuePage />} />
+        <Route path="/ThemePage" element={<ThemePage />} />
+        <Route path="/FoodPage" element={<FoodPage />} />
+        <Route path="/PhotographyPage" element={<PhotographyPage />} />
+      </Routes>
+      <div className="h-[10vh]"/>
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
