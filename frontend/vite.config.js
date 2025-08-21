40f24d33992aacb9b5e6c0077c5b5ca841330e38
avadhesh11 <202452309@iiitvadodara.ac.in>
2025-08-21 21:12:39 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/GROUP-PROJECT-1/',
+   base: './',
   plugins: [
     react(),
     tailwindcss()
